0a13e6f15fb7fa97b032b39747ac33b735d65a08
sanghoon <hankora817@gmail.com>
2017-03-06 15:38:04 +0900

    - sound bar

--- app/src/main/java/com/hankora817/vlcplayersample/MainActivity.java
+++ app/src/main/java/com/hankora817/vlcplayersample/MainActivity.java
@@ -3,7 +3,6 @@ package com.hankora817.vlcplayersample;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +19,7 @@ public class MainActivity extends Activity
 		Button b = (Button) findViewById(R.id.play_video);
 		final EditText e = (EditText) findViewById(R.id.video_url);
 		e.setText("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+//		e.setText("https://victony.jp.quickconnect.to/fbdownload/math_1.mp4?_sid=%22GPXRCRUTnhYm.1710O1N559600%22&mode=open&dlink=%222f61636164656d79636b2f6d6174685f312e6d7034%22&stdhtml=true&SynoToken=Gn2IF0lfrNNnA");
 		
 		b.setOnClickListener(new View.OnClickListener()
 		{
--- app/src/main/java/com/hankora817/vlcplayersample/VideoVLCActivity.java
+++ app/src/main/java/com/hankora817/vlcplayersample/VideoVLCActivity.java
@@ -9,18 +9,27 @@ import org.videolan.libvlc.Media;
 import org.videolan.libvlc.MediaPlayer;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Configuration;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.KeyEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
-public class VideoVLCActivity extends Activity implements IVLCVout.Callback
+public class VideoVLCActivity extends Activity implements IVLCVout.Callback, Handler.Callback
 {
+	private static final int SHOW_PROGRESS = 0;
 	private LibVLC libvlc;
 	private MediaPlayer mMediaPlayer = null;
 	
@@ -28,6 +37,14 @@ public class VideoVLCActivity extends Activity implements IVLCVout.Callback
 	private FrameLayout mSurfaceFrame;
 	private SurfaceHolder mSurfaceHolder;
 	private Surface mSurface = null;
+	
+	private ImageButton mBtnPlay;
+	private SeekBar mSeekBar;
+	private VerticalSeekBar mSeekSound;
+	
+	private AudioManager mAudioManger;
+	
+	private Handler mHandler;
 	
 	private int mVideoWidth;
 	private int mVideoHeight;
@@ -39,10 +56,23 @@ public class VideoVLCActivity extends Activity implements IVLCVout.Callback
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_player);
 		
+		mAudioManger = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		
 		mSurfaceView = (SurfaceView) findViewById(R.id.surface);
 		mSurfaceHolder = mSurfaceView.getHolder();
 		
 		mSurfaceFrame = (FrameLayout) findViewById(R.id.player_surface_frame);
+		
+		mSeekBar = (SeekBar) findViewById(R.id.seekbar);
+		mSeekSound = (VerticalSeekBar) findViewById(R.id.seek_sound);
+		mSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+		mSeekSound.setOnSeekBarChangeListener(seekBarChangeListener);
+		
+		mBtnPlay = (ImageButton) findViewById(R.id.btn_play);
+		findViewById(R.id.btn_sound).setOnClickListener(btnClickListener);
+		mBtnPlay.setOnClickListener(btnClickListener);
+		
+		mHandler = new Handler(this);
 		
 		createPlayer(getIntent().getStringExtra("videoUrl"));
 	}
@@ -85,6 +115,7 @@ public class VideoVLCActivity extends Activity implements IVLCVout.Callback
 		if (libvlc == null)
 			return;
 		mMediaPlayer.stop();
+		mHandler.removeMessages(SHOW_PROGRESS);
 		final IVLCVout vout = mMediaPlayer.getVLCVout();
 		vout.removeCallback(this);
 		vout.detachViews();
@@ -95,7 +126,77 @@ public class VideoVLCActivity extends Activity implements IVLCVout.Callback
 		mVideoHeight = 0;
 	}
 	
+	
+	private int setSeekProgress()
+	{
+		if (mMediaPlayer == null)
+			return 0;
+		int max = (int) mMediaPlayer.getLength();
+		int time = (int) mMediaPlayer.getTime();
+		mSeekBar.setMax(max);
+		mSeekBar.setProgress(time);
+		return time;
+	}
+	
 	private MediaPlayer.EventListener mPlayerListener = new MyPlayerListener(this);
+	
+	
+	@Override
+	public boolean handleMessage(Message msg)
+	{
+		switch (msg.what)
+		{
+			case SHOW_PROGRESS:
+				setSeekProgress();
+				mHandler.sendEmptyMessageDelayed(SHOW_PROGRESS, 20);
+				break;
+		}
+		return false;
+	}
+	
+	private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener()
+	{
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+		{
+			if (seekBar.getId() == R.id.seekbar)
+			{
+				if (fromUser)
+				{
+					mMediaPlayer.setTime(progress);
+					setSeekProgress();
+				}
+			}
+			else if (seekBar.getId() == R.id.seek_sound)
+			{
+				if (fromUser)
+				{
+					mAudioManger.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+				}
+			}
+		}
+		
+		
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar)
+		{
+			
+		}
+		
+		
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar)
+		{
+			new Handler().postDelayed(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					mSeekSound.setVisibility(View.INVISIBLE);
+				}
+			}, 500);
+		}
+	};
 	
 	private static class MyPlayerListener implements MediaPlayer.EventListener
 	{
@@ -119,6 +220,8 @@ public class VideoVLCActivity extends Activity implements IVLCVout.Callback
 					player.releasePlayer();
 					break;
 				case MediaPlayer.Event.Playing:
+					player.mHandler.sendEmptyMessage(SHOW_PROGRESS);
+					player.mBtnPlay.setBackgroundResource(android.R.drawable.ic_media_pause);
 					break;
 				case MediaPlayer.Event.Paused:
 					break;
@@ -213,5 +316,60 @@ public class VideoVLCActivity extends Activity implements IVLCVout.Callback
 	{
 		super.onDestroy();
 		releasePlayer();
+	}
+	
+	private View.OnClickListener btnClickListener = new View.OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			switch (v.getId())
+			{
+				case R.id.btn_play:
+					if (mMediaPlayer.isPlaying())
+					{
+						mMediaPlayer.pause();
+						mBtnPlay.setBackgroundResource(android.R.drawable.ic_media_play);
+					}
+					else
+					{
+						mMediaPlayer.play();
+						mBtnPlay.setBackgroundResource(android.R.drawable.ic_media_pause);
+					}
+					break;
+				case R.id.btn_sound:
+					if (mSeekSound.isShown())
+						mSeekSound.setVisibility(View.INVISIBLE);
+					else
+					{
+						mSeekSound.setMax(mAudioManger.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+						mSeekSound.setProgress(mAudioManger.getStreamVolume(AudioManager.STREAM_MUSIC));
+						mSeekSound.updateThumb();
+						mSeekSound.setVisibility(View.VISIBLE);
+					}
+					break;
+			}
+		}
+	};
+	
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+		if (event.getAction() == KeyEvent.ACTION_DOWN)
+		{
+			switch (keyCode)
+			{
+				case KeyEvent.KEYCODE_VOLUME_UP:
+					mSeekSound.setProgress(mAudioManger.getStreamVolume(AudioManager.STREAM_MUSIC));
+					mSeekSound.updateThumb();
+					break;
+				case KeyEvent.KEYCODE_VOLUME_DOWN:
+					mSeekSound.setProgress(mAudioManger.getStreamVolume(AudioManager.STREAM_MUSIC));
+					mSeekSound.updateThumb();
+					break;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 }
